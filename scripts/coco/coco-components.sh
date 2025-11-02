39ba64ce59d17fb96d769f00a201476038913057
Snir Schreiber <ssheribe@redhat.com>
2025-11-02 15:06:24 +0200

    local: increase memory size for virt-customize
    needed for driver build at installtion

--- scripts/coco/coco-components.sh
+++ scripts/coco/coco-components.sh
@@ -82,7 +82,7 @@ EXTRA_ARGS=""
 [[ -n "$ROOT_PASSWORD" ]] && EXTRA_ARGS=" --root-password password:${ROOT_PASSWORD} "
 tmp_podvm_maker=$(mktemp --suffix='.sh')
 envsubst '$ORG_ID $ACTIVATION_KEY' <  $ARTIFACTS_FOLDER/podvm_maker.sh > $tmp_podvm_maker
-virt-customize \
+virt-customize --memsize 8192 \
     --copy-in $ARTIFACTS_FOLDER/podvm-binaries.tar.gz:/tmp/ \
     --copy-in $ARTIFACTS_FOLDER/pause-bundle.tar.gz:/tmp/ \
     --copy-in $ARTIFACTS_FOLDER/luks-config.tar.gz:/tmp/ \
